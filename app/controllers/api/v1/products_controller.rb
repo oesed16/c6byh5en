d939e8dee7db489dee9a1f7e4424255cfd198152
Angela Castagno <oesed16@gmail.com>
2018-12-18 13:40:53 -0500

    Listar productos con JSON en URL específica para API incluyendo recomendaciones de solución

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,28 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.create(product_params)
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.update(params[:id], product_params)
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price)
+    render json: Product.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
       namespace :v1 do
-        resources :products
+        resources :products, only: [:index]
       end
     end
-    
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
