e831229d0871f281080305317d3f4ec30bc4c424
Angela Castagno <oesed16@gmail.com>
2018-12-18 14:05:46 -0500

    Crear productos con JSON en URL específica para la API

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render json: Product.all
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created  
+    else
+      render json: @product.errors, status: :unprocessable_entity  
+    end
+  end
+  
+  private
+
+	def product_params
+	  params.require(:product).permit(:name, :price)
+  end
+  
 end
